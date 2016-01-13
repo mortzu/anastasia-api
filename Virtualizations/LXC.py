96dc28d747d1ff01db498191107726b6285ce6ed
mortzu <mortzu@gmx.de>
2016-01-13 06:55:45 +0100

    Added support for unprivileged containers

--- Virtualizations/LXC.py
+++ Virtualizations/LXC.py
@@ -3,16 +3,16 @@ import socket
 import pwd
 import glob
 import os
+import subprocess
+import json
 
 class LXC:
-    container_path = "/var/lib/lxc/"
-    cgroup_path = "/sys/fs/cgroup/"
+    container_path = '/var/lib/lxc/'
+    unprivileged_container_path = '/var/lib/vps/'
+    cgroup_path = '/sys/fs/cgroup/'
 
     def __init__(self, libvirt_uri = None, enable_ssh_console = False):
         self.enable_ssh_console = enable_ssh_console
-
-    def pages2mb(self, pages):
-        return pages * 4096 / 1024 / 1024
 
     def get_domains(self):
         # dictionary for result
@@ -21,33 +21,56 @@ class LXC:
         result['hostname'] = socket.getfqdn()
         result['hypervisor'] = 'LXC'
 
-        for container in glob.glob(self.container_path + '*/config'):
-            domain_name = container.replace(self.container_path, "").rstrip("/config")
+        for container in glob.glob(self.container_path + '*/config') + glob.glob(self.unprivileged_container_path + 'vps*/.local/share/lxc/*/config'):
+            domain_name = os.path.basename(container.rstrip('/config'))
             result[domain_name] = {}
+            result[domain_name]['id'] = 0
+            result[domain_name]['vcpu'] = 0
+            result[domain_name]['name'] = domain_name
 
-            cont = lxc.Container(domain_name)
+            if self.unprivileged_container_path in container:
+                lxc_username = os.path.basename(os.path.dirname(container.rstrip('/config')).rstrip('/.local/share/lxc'))
 
-            if self.enable_ssh_console:
                 try:
-                    pwd.getpwnam('vps' + str(ct['ctid']))
+                    pwd.getpwnam(lxc_username)
+                except:
+                    continue
+
+                proc = subprocess.Popen('/usr/bin/sudo -Hu ' + lxc_username + ' /opt/anastasia-api/lxc-info-json.py ' + domain_name, shell = True, stdout = subprocess.PIPE)
+                data = json.loads(proc.communicate()[0])
+
+                if self.enable_ssh_console:
                     result[domain_name]['console_type'] = 'SSH'
                     result[domain_name]['console_address'] = result['hostname']
                     result[domain_name]['console_port'] = 22
-                except KeyError:
-                    result[domain_name]['console_type'] = None
-                    result[domain_name]['console_address'] = None
-                    result[domain_name]['console_port'] = None
+                    result[domain_name]['console_username'] = lxc_username
 
-            result[domain_name]['id'] = 0
-            result[domain_name]['state'] = cont.state.swapcase()
-            result[domain_name]['name'] = domain_name
+                result[domain_name]['unprivileged'] = True
+                result[domain_name]['state'] = data['state']
+                result[domain_name]['memory'] = data['memory']
+                result[domain_name]['ip'] = data['ip']
+            else:
+                cont = lxc.Container(domain_name)
 
-            try:
-                result[domain_name]['memory'] = int(open(self.cgroup_path + "memory/lxc/" + domain_name + "/memory.limit_in_bytes", 'r').read()) / 1024
-            except:
-                result[domain_name]['memory'] = 0
-            result[domain_name]['vcpu'] = 0
-            result[domain_name]['ip'] = cont.get_ips(timeout=5)
+                result[domain_name]['unprivileged'] = False
+                result[domain_name]['console_type'] = None
+                result[domain_name]['console_address'] = None
+                result[domain_name]['console_port'] = None
+                result[domain_name]['console_username'] = None
+
+                result[domain_name]['state'] = cont.state.swapcase()
+
+                try:
+                    result[domain_name]['memory'] = int(open(self.cgroup_path + 'memory/lxc/' + domain_name + '/memory.limit_in_bytes', 'r').read()) / 1024
+                except:
+                    result[domain_name]['memory'] = 0
+
+                try:
+                    result[domain_name]['swap'] = (int(open(self.cgroup_path + 'memory/lxc/' + domain_name + '/memory.memsw.limit_in_bytes', 'r').read()) / 1024) - result['memory']
+                except:
+                    result[domain_name]['swap'] = 0
+
+                result[domain_name]['ip'] = cont.get_ips(timeout = 5)
 
         return result
 
---
+++ lxc-info-json.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python2
+
+import sys
+import lxc
+import json
+import os
+import getpass
+
+cgroup_path = '/sys/fs/cgroup/'
+unprivileged = False
+
+try:
+    domain_name = sys.argv[1]
+except:
+    print 'Argument not given'
+    sys.exit(1)
+
+if os.getuid != 0:
+    unprivileged = True
+
+cont = lxc.Container(sys.argv[1])
+
+result = {}
+result['state'] = cont.state.swapcase()
+
+try:
+    if unprivileged == True:
+        result['memory'] = int(open(cgroup_path + 'memory/' + getpass.getuser() + '/lxc/' + domain_name + '/memory.limit_in_bytes', 'r').read()) / 1024
+        result['swap'] = (int(open(cgroup_path + 'memory/' + getpass.getuser() + '/lxc/' + domain_name + '/memory.memsw.limit_in_bytes', 'r').read()) / 1024) - result['memory']
+    else:
+        result['memory'] = int(open(cgroup_path + 'memory/lxc/' + domain_name + '/memory.limit_in_bytes', 'r').read()) / 1024
+        result['swap'] = (int(open(cgroup_path + 'memory/lxc/' + domain_name + '/memory.memsw.limit_in_bytes', 'r').read()) / 1024) - result['memory']
+except:
+    result['memory'] = 0
+
+result['vcpu'] = 0
+result['ip'] = cont.get_ips(timeout = 5)
+
+print json.dumps(result, separators = (',', ':'))

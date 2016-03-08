009a9d0029085df1b7e1d42f4d06101eac1ff092
mortzu <mortzu@gmx.de>
2016-03-08 04:45:39 +0100

    Removed support for unprivileged containers

--- Virtualizations/LXC.py
+++ Virtualizations/LXC.py
@@ -3,13 +3,9 @@ import socket
 import pwd
 import glob
 import os
-import subprocess
-from subprocess import PIPE
-import json
 
 class LXC:
     container_path = '/var/lib/lxc/'
-    unprivileged_container_path = '/var/lib/vps/'
     cgroup_path = '/sys/fs/cgroup/'
 
     def __init__(self, cli_args = None):
@@ -19,56 +15,34 @@ class LXC:
         # dictionary for result
         result = {}
 
-        for container in glob.glob(self.container_path + '*/config') + glob.glob(self.unprivileged_container_path + 'vps*/.local/share/lxc/*/config'):
+        for container in glob.glob(self.container_path + '*/config'):
             domain_name = os.path.basename(container.rstrip('/config'))
             result[domain_name] = {}
             result[domain_name]['id'] = 0
             result[domain_name]['vcpu'] = 0
             result[domain_name]['name'] = domain_name
 
-            if self.unprivileged_container_path in container:
-                lxc_username = os.path.basename(os.path.dirname(container.rstrip('/config')).rstrip('/.local/share/lxc'))
+            cont = lxc.Container(domain_name)
 
-                try:
-                    pwd.getpwnam(lxc_username)
-                except:
-                    continue
+            result[domain_name]['unprivileged'] = False
+            result[domain_name]['console_type'] = None
+            result[domain_name]['console_address'] = None
+            result[domain_name]['console_port'] = None
+            result[domain_name]['console_username'] = None
 
-                proc = subprocess.Popen(['/usr/bin/sudo', '-Hu', lxc_username, '/opt/anastasia-api/lxc-info-json.py', domain_name], stdout = PIPE)
-                data = json.loads(proc.communicate()[0])
+            result[domain_name]['state'] = cont.state.swapcase()
 
-                if self.enable_ssh_console:
-                    result[domain_name]['console_type'] = 'SSH'
-                    result[domain_name]['console_address'] = result['hostname']
-                    result[domain_name]['console_port'] = 22
-                    result[domain_name]['console_username'] = lxc_username
+            try:
+                result[domain_name]['memory'] = int(open(self.cgroup_path + 'memory/lxc/' + domain_name + '/memory.limit_in_bytes', 'r').read()) / 1024
+            except:
+                result[domain_name]['memory'] = 0
 
-                result[domain_name]['unprivileged'] = True
-                result[domain_name]['state'] = data['state']
-                result[domain_name]['memory'] = data['memory']
-                result[domain_name]['ip_assignment'] = data['ip_assignment']
-            else:
-                cont = lxc.Container(domain_name)
+            try:
+                result[domain_name]['swap'] = (int(open(self.cgroup_path + 'memory/lxc/' + domain_name + '/memory.memsw.limit_in_bytes', 'r').read()) / 1024) - result['memory']
+            except:
+                result[domain_name]['swap'] = 0
 
-                result[domain_name]['unprivileged'] = False
-                result[domain_name]['console_type'] = None
-                result[domain_name]['console_address'] = None
-                result[domain_name]['console_port'] = None
-                result[domain_name]['console_username'] = None
-
-                result[domain_name]['state'] = cont.state.swapcase()
-
-                try:
-                    result[domain_name]['memory'] = int(open(self.cgroup_path + 'memory/lxc/' + domain_name + '/memory.limit_in_bytes', 'r').read()) / 1024
-                except:
-                    result[domain_name]['memory'] = 0
-
-                try:
-                    result[domain_name]['swap'] = (int(open(self.cgroup_path + 'memory/lxc/' + domain_name + '/memory.memsw.limit_in_bytes', 'r').read()) / 1024) - result['memory']
-                except:
-                    result[domain_name]['swap'] = 0
-
-                result[domain_name]['ip_assignment'] = cont.get_ips(timeout = 2)
+            result[domain_name]['ip_assignment'] = cont.get_ips(timeout = 2)
 
             result[domain_name]['hypervisor'] = 'LXC'
             result[domain_name]['hostname'] = socket.getfqdn()
